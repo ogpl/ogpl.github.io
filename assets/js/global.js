aba6a441ad2813c57789ee69af0c49080a5996ef
Paul Jackson <Paul.Jackson@tbs-sct.gc.ca>
2013-06-09 00:41:44 -0400

    Fixed edit fallback selector.

--- assets/js/global.js
+++ assets/js/global.js
@@ -3,7 +3,7 @@
 	"use strict";
 	// Test that browser supports CORS (uses test from prose/boot.js). Intended to reroute to fallback when Prose won't work. 
 	if (!('withCredentials' in new XMLHttpRequest())) {
-		var $editLink = $('edit-page'),
+		var $editLink = $('#edit-page'),
 			request = false;
 		if ($editLink.length !== 0) {
 			$editLink.attr('href', $editLink.attr('href').replace('http://prose.io/#ogpl/', 'https://github.com/ogpl/'));
