a7f9847762c2b5ccbb8625ce8afe6bb55aa00478
Paul Jackson <Paul.Jackson@tbs-sct.gc.ca>
2013-09-17 13:26:17 -0400

    Updated to the Web Experience Toolkit v3.1.4 maintenance release.

--- wet-boew/dist/js/settings.js
+++ wet-boew/dist/js/settings.js
@@ -3,12 +3,14 @@
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
  *
- * Version: v3.1.4-development Build: 2013-08-21 12:06 PM EDT
+ * Version: v3.1.4-release Build: 2013-09-17 12:23 PM EDT
  *
  */
 /*
 WET-BOEW-Settings
  */
+
+/* exported wet_boew_properties, wet_boew_share */
 var wet_boew_properties = {
 	/** global plugins are called via a array of dependency names **/
 	"globals" : ["deselectradio", "datemodified"]
